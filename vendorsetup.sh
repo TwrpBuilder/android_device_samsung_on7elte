e73ac00bcd1d3c32ddf67387828397474ca3df4d
grace5921 <androidlover5842@gmail.com>
2016-12-26 14:10:00 +0530

    initial commit

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+add_lunch_combo omni_on7elte-eng
+function mkazip_on7elte
+{
+cp device/samsung/on7elte/META-INF.zip out/target/product/on7elte/META-INF.zip 
+cd out/target/product/on7elte/
+unzip META-INF.zip 
+rm META-INF.zip 
+zip twrp-$(date +%Y%m%d)-3.0.2-UNOFFICIAL-on7elte.zip  -r recovery.img -r META-INF
+cd ../../../../
+}
