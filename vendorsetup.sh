d2b3ac1a93ccc0ad3206fec3b41e94b7f0206887
grace5921 <androidlover5842@gmail.com>
2016-12-31 16:04:37 +0530

    fix build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@ cp device/samsung/on7elte/META-INF.zip out/target/product/on7elte/META-INF.zip
 cd out/target/product/on7elte/
 unzip META-INF.zip 
 rm META-INF.zip 
-zip twrp-$(date +%Y%m%d)-3.0.2-UNOFFICIAL-on7elte.zip  -r recovery.img -r META-INF
+zip twrp-$(date +%Y%m%d)-3.0.3-UNOFFICIAL-on7elte.zip  -r recovery.img -r META-INF
 cd ../../../../
 }
